7909d3f0e0372a010c41eecadce84a796d0235ee
DaveAimee <liuy7937@gmail.com>
2018-11-21 19:14:22 +0800

    add some contents.

---
+++ Python Fundamentals/At05_Loop statement.py
@@ -0,0 +1,23 @@
+# 循环
+# ①一种是for...in循环，依次把list或tuple中的每个元素迭代出来
+# 例如：
+# names = ['Michael', 'Bob', 'Tracy']
+# for name in names:
+#     print(name)
+# ②另外一种为while循环，只要条件满足，就不断循环
+# 例如：
+# sum = 0
+# n = 99
+# while n > 0:
+#     sum = sum + n
+#     n = n - 2
+# print(sum)
+
+L = ['Bart', 'Lisa', 'Adam']
+for name in L:
+    print('Hello, %s' % name)
+
+
+
+
+
