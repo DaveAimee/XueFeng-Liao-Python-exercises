a4a299b28401f517b6e99d11e50d01516c57f1eb
DaveAimee <liuy7937@gmail.com>
2018-12-12 19:33:38 +0800

    add some contents.

--- Python Fundamentals/At60_Coroutine.py
+++ Python Fundamentals/At60_Coroutine.py
@@ -1,43 +1,22 @@
-# def consumer():
-#     r = ''
-#     while True:
-#         n = yield r
-#         if not n:
-#             return
-#         print('[CONSUMER] Consuming %s...' % n)
-#         r = '200 OK'
-#
-# def produce(c):
-#     c.send(None)
-#     n = 0
-#     while n < 5:
-#         n = n + 1
-#         print('[PRODUCER] Producing %s...' % n)
-#         r = c.send(n)
-#         print('[PRODUCER] Consumer return: %s' % r)
-#     c.close()
-#
-# c = consumer()
-# produce(c)
-def echo(value=None):
-    print("Execution starts when 'next()' is called for the first time.")
-    try:
-        while True:
-            try:
-                value = (yield value)
-            except Exception as e:
-                value = e
-    finally:
-        print("Don't forget to clean up when 'close()' is called.")
+def consumer():
+    r = ''
+    while True:
+        n = yield r
+        if not n:
+            return
+        print('[CONSUMER] Consuming %s...' % n)
+        r = '200 OK'
 
-generator = echo(1)
-print(next(generator))
+def produce(c):
+    c.send(None)
+    n = 0
+    while n < 5:
+        n = n + 1
+        print('[PRODUCER] Producing %s...' % n)
+        r = c.send(n)
+        print('[PRODUCER] Consumer return: %s' % r)
+    c.close()
 
+c = consumer()
+produce(c)
 
-print(next(generator))
-
-print(generator.send(2))
-
-generator.throw(TypeError, "spam")
-
-generator.close()
